321fda1fb3abe6b7903681ace0b658d911ab13ca
OHTAKE Tomohiro <tomohiro@kuramae.ne.jp>
2017-04-04 23:13:45 +0900

    List all areas
    Least significant digit may be different among areas

--- generate_download_script.sh
+++ generate_download_script.sh
@@ -7,10 +7,14 @@
 #   where v-low-tokyo1   : area
 #         2865           : dir
 #         11             : sequence (00-99)
-#         20170402092959 : start (the last number should be either 4 or 9)
+#         20170402092959 : start
 #         360            : max (e.g. 30min => 30*60/5 => 360)
 #
-# To list 100-latest ts files, curl https://smartcast.hs.llnwd.net/v-low-tokyo1/2865/2865.txt
+# To list 100-latest ts files, curl either of:
+#   https://smartcast.hs.llnwd.net/v-low-tokyo1/2865/2865.txt
+#   https://smartcast.hs.llnwd.net/v-low-nagoya1/2C65/2C65.txt
+#   https://smartcast.hs.llnwd.net/v-low-osaka1/3065/3065.txt
+#   https://smartcast.hs.llnwd.net/v-low-fukuoka1/3865/3865.txt
 
 area=$1
 dir=$2
