4b4c120115143f73e960d1000fc9c637d245bd53
OHTAKE Tomohiro <tomohiro@kuramae.ne.jp>
2017-04-04 19:00:43 +0900

    initial commit

---
+++ generate_concat_script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Example
+#   ./generate_concat_script.sh > temp-concat.sh
+#   bash temp-concat.sh
+#
+# Requirement
+#   ffmpeg <http://ffmpeg.org/>
+
+echo -e "ffmpeg -i concat:'\c"
+
+isFirst=1
+for ts in $(ls *.ts | sort); do
+  if [ $isFirst -eq 1 ]; then
+    isFirst=0
+  else
+    echo -e '|\c'
+  fi
+  echo -e "${ts}\c"
+done
+
+echo -e "' -c copy output.mp4\c"
+echo
---
+++ generate_download_script.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Example
+#   ./generate_download_script.sh v-low-tokyo1 2865 11 20170402092959 360 > temp-download.sh
+#   bash temp-download.sh
+#
+#   where v-low-tokyo1   : area
+#         2865           : dir
+#         11             : sequence (00-99)
+#         20170402092959 : start (the last number should be either 4 or 9)
+#         360            : max (e.g. 30min => 30*60/5 => 360)
+#
+# To list 100-latest ts files, curl https://smartcast.hs.llnwd.net/v-low-tokyo1/2865/2865.txt
+
+area=$1
+dir=$2
+seq=$3
+ts=$4
+max=$5
+
+echo 'set -o errexit'
+for ((i=0; i < $max; i++)); do
+  url=$(printf 'curl https://smartcast.hs.llnwd.net/%s/%d/%02s_%s.ts -o %s.ts' $area $dir $seq $ts $ts)
+  echo $url
+
+  seq=$(($seq + 1))
+  if [ $seq -eq 100 ]; then
+    seq="0"
+  fi
+  ts=$(($ts + 5))
+  if [ $(($ts % 100)) -ge 60 ]; then
+    ts=$(($ts - 60 + 100))
+    if [ $(($ts % 10000)) -ge 6000 ]; then
+      ts=$(($ts - 6000 + 10000))
+      if [ $(($ts % 1000000)) -ge 240000 ]; then
+        ts=$(($ts - 240000 + 1000000))
+        # FIXME: Increment month, and year
+      fi
+    fi
+  fi
+done
