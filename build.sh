1f1a72bf65af5014f36e834b102ebb9e09516b5c
OHTAKE Tomohiro <tomohiro@kuramae.ne.jp>
2017-04-05 14:24:38 +0900

    Add build script

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+mkdir -p out
+GOOS=linux GOARCH=amd64 go build -o out/save_stream.linux-amd64 save_stream.go
+GOOS=windows GOARCH=amd64 go build -o out/save_stream.windows-amd64.exe save_stream.go
+GOOS=windows GOARCH=386 go build -o out/save_stream.windows-386.exe save_stream.go
