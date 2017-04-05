d4e8dc8d1041a208c822f88eeaa3872227d17289
OHTAKE Tomohiro <tomohiro@kuramae.ne.jp>
2017-04-05 23:02:18 +0900

    Report coverage

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,12 @@ set -o nounset
 set -o pipefail
 
 git checkout -B gh-pages
+
+mkdir -p out
+GOOS=linux GOARCH=amd64 go build -o out/save_stream.linux-amd64 save_stream.go
+GOOS=windows GOARCH=amd64 go build -o out/save_stream.windows-amd64.exe save_stream.go
+GOOS=windows GOARCH=386 go build -o out/save_stream.windows-386.exe save_stream.go
+
 git add out -f
 
 if [ "true" = "${TRAVIS}" ]; then
