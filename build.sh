cc1b852ef686db6aad69a903d6085385a6f397b4
OHTAKE Tomohiro <ohtake.tomohiro@jp.fujitsu.com>
2019-04-10 15:15:04 +1000

    Remove win32 target and add Linux arm64 target

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ mkdir -p out
 
 cp scripts/* out/
 
+GOOS=linux   GOARCH=arm64 go build -o out/save_stream.linux-arm64.out   save_stream.go
 GOOS=linux   GOARCH=amd64 go build -o out/save_stream.linux-amd64.out   save_stream.go
 GOOS=windows GOARCH=amd64 go build -o out/save_stream.windows-amd64.exe save_stream.go
-GOOS=windows GOARCH=386   go build -o out/save_stream.windows-386.exe   save_stream.go
