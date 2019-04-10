c200ae1eacc2c6757dcf9571208f06289b066434
OHTAKE Tomohiro <ohtake.tomohiro@jp.fujitsu.com>
2019-04-10 15:17:07 +1000

    Use dist directory for distribution

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-mkdir -p out
+mkdir -p dist
 
-cp scripts/* out/
+cp scripts/* dist/
 
-GOOS=linux   GOARCH=arm64 go build -o out/save_stream.linux-arm64.out   save_stream.go
-GOOS=linux   GOARCH=amd64 go build -o out/save_stream.linux-amd64.out   save_stream.go
-GOOS=windows GOARCH=amd64 go build -o out/save_stream.windows-amd64.exe save_stream.go
+GOOS=linux   GOARCH=arm64 go build -o dist/save_stream.linux-arm64.out   save_stream.go
+GOOS=linux   GOARCH=amd64 go build -o dist/save_stream.linux-amd64.out   save_stream.go
+GOOS=windows GOARCH=amd64 go build -o dist/save_stream.windows-amd64.exe save_stream.go
